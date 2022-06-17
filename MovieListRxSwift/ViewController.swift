a07d460a0ee4362913dc0213aa95a812f2291cb5
Khavi18 <khavishini18@gmail.com>
2022-06-17 16:36:33 +0800

    Initial Commit

---
+++ MovieListRxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieListRxSwift
+//
+//  Created by khavishini suresh on 17/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
